b3eb1edbb4fe4d76b108854e27aa4395e9d03e62
Betty <alzbeta.parobkova@gmail.com>
2024-07-16 18:47:24 +0200

    final version of model

--- eda.py
+++ eda.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.preprocessing import LabelEncoder
@@ -8,65 +9,83 @@ test = pd.read_csv("datasets/test.csv")
 train = pd.read_csv("datasets/train.csv")
 
 
-def eda(train):
-    print(train.head())
-    print(train.columns)
-    print(train.dtypes)
-    correlation = train.corr()
-    print(correlation)
-    sns.heatmap(correlation, annot=True)
-    plt.show()
+print(train.head())
+print(train.columns)
+print(train.dtypes)
 
-    for columns in test:
-        print(train[columns].value_counts())
+for columns in test:
+    print(train[columns].value_counts())
 
-    # Exploring the data in general by looking at its structure
-    info = train.describe(include="all")
-    null_values = train.isnull().sum().sort_values(ascending=False)
-    # print(null_values)
+# Exploring the data in general by looking at its structure
+info = train.describe(include="all")
+null_values = train.isnull().sum().sort_values(ascending=False)
+# print(null_values)
 
-    ## Looking at categories through bar charts
-    categoricalCols = [
-        "Item_Fat_Content",
-        "Item_Type",
-        "Outlet_Identifier",
-        "Outlet_Size",
-        "Outlet_Location_Type",
-        "Outlet_Type",
-    ]
-    categoriesTempDf = train[categoricalCols]
-    catList = categoriesTempDf.columns.tolist()
+## Looking at categories through bar charts
+categoricalCols = [
+    "Item_Fat_Content",
+    "Item_Type",
+    "Outlet_Identifier",
+    "Outlet_Size",
+    "Outlet_Location_Type",
+    "Outlet_Type",
+]
+categoriesTempDf = train[categoricalCols]
+catList = categoriesTempDf.columns.tolist()
 
-    for catCols in categoriesTempDf:
-        plt.figure()
-        catCount = categoriesTempDf[catCols].value_counts()
-        catCount.plot(kind="bar")
-        plt.xlabel(catCols)
-        plt.ylabel("Count")
-        # plt.show()
+for catCols in categoriesTempDf:
+    # plt.figure()
+    catCount = categoriesTempDf[catCols].value_counts()
+    catCount.plot(kind="bar")
+    plt.xlabel(catCols)
+    plt.ylabel("Count")
+    # plt.show()
 
-    ## Preprocessing and data preparation
-    train["Item_Fat_Content"] = train["Item_Fat_Content"].replace(
-        {
-            "Low Fat": "Low Fat",
-            "LF": "Low Fat",
-            "low fat": "Low Fat",
-            "Regular": "Regular",
-            "reg": "Regular",
-        }
-    )
+## Preprocessing and data preparation
+# Standardization of category names
+train["Item_Fat_Content"] = train["Item_Fat_Content"].replace(
+    {
+        "Low Fat": "Low Fat",
+        "LF": "Low Fat",
+        "low fat": "Low Fat",
+        "Regular": "Regular",
+        "reg": "Regular",
+    }
+)
 
-    train["Outlet_Size"] = train["Outlet_Size"].fillna(train["Outlet_Size"].mode()[0])
-    train["Item_Weight"] = train["Item_Weight"].fillna(train["Item_Weight"].mean())
+# Removing null values
+train["Outlet_Size"] = train["Outlet_Size"].fillna(train["Outlet_Size"].mode()[0])
+train["Item_Weight"] = train["Item_Weight"].fillna(train["Item_Weight"].mean())
 
-    for catCols in catList:
-        train[catCols] = LabelEncoder().fit_transform(train[catCols])
+# Transforming categorical data to numerical values
+for catCols in catList:
+    train[catCols] = LabelEncoder().fit_transform(train[catCols])
 
-    return train
+# Finding correlation between variables in order to determine right features for model
+correlation = train.corr()
+print(correlation)
+sns.heatmap(correlation, annot=True)
+plt.show()
 
+# Identifying and removing outliers
+numCols = ["Item_Weight", "Item_Visibility", "Item_MRP"]
+sns.boxplot(train[numCols])
+plt.show()
 
-train = eda(train)
-test = eda(test)
+Q1 = train["Item_Visibility"].quantile(0.25)
+Q3 = train["Item_Visibility"].quantile(0.75)
+IQR = Q3 - Q1
+lower = Q1 - 1.5 * IQR
+upper = Q3 + 1.5 * IQR
 
+upperThreshold = np.where(train["Item_Visibility"] >= upper)[0]
+lowerThreshold = np.where(train["Item_Visibility"] <= lower)[0]
+
+# Removing the outliers
+train.drop(index=upperThreshold, inplace=True)
+train.drop(index=lowerThreshold, inplace=True)
+sns.boxplot(train["Item_Visibility"])
+plt.show()
+
+## Saving preprocesed dataset
 train.to_csv("datasets/train2.csv")
-test.to_csv("datasets/test2.csv")
--- model.py
+++ model.py
@@ -1,14 +1,21 @@
-import numpy as np
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 import sklearn.metrics as m
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import (
+    train_test_split,
+    RandomizedSearchCV,
+    cross_val_score,
+)
+from sklearn.ensemble import RandomForestRegressor
 import pickle
 
 data = pd.read_csv("datasets/train2.csv")
+
+
+## Model
 # model = LinearRegression
-X = data[["Item_Weight", "Item_Visibility", "Item_MRP", "Outlet_Establishment_Year"]]
+X = data.drop(columns=["Item_Outlet_Sales", "Item_Identifier"], axis=1)
 y = data["Item_Outlet_Sales"]
 X_train, X_test, y_train, y_test = train_test_split(
     X,
@@ -16,12 +23,38 @@ X_train, X_test, y_train, y_test = train_test_split(
     test_size=0.2,
     random_state=42,
 )
-model = LinearRegression()
+model = RandomForestRegressor(n_estimators=50, min_samples_leaf=4, bootstrap=True)
 model.fit(X_train, y_train)
-modelPrediction = model.predict(X_test)
-modelMeanAbsErr = m.mean_absolute_error(y_test, modelPrediction)
-modelMeanSqrtErr = m.mean_squared_error(y_test, modelPrediction)
-modelR2Score = m.r2_score(y_test, modelPrediction)
+predictionModel = model.predict(X_test)
+
+## Hypertuning parametres for better model perferomance
+parameters = {
+    "bootstrap": [True, False],
+    "n_estimators": [25, 50, 100, 150],
+    "max_features": ["sqrt", "log2", None],
+    "min_samples_leaf": [1, 2, 4],
+}
+
+randomSearch = RandomizedSearchCV(
+    estimator=model,
+    param_distributions=parameters,
+    n_iter=30,
+    cv=3,
+    verbose=2,
+    random_state=42,
+    n_jobs=-1,
+)
+
+randomSearch.fit(X, y)
+print(randomSearch.best_params_)
+print(randomSearch.best_score_)
+
+## Model evaluation
+modelMeanAbsErr = m.mean_absolute_error(y_test, predictionModel)
+modelMeanSqrtErr = m.mean_squared_error(y_test, predictionModel)
+modelR2Score = m.r2_score(y_test, predictionModel)
+cv_scores = cross_val_score(model, X, y, cv=3, scoring="neg_mean_squared_error")
+rmse_scores = np.sqrt(-cv_scores)
 
 print(
     "Model Mean Absolute Error:",
@@ -30,8 +63,15 @@ print(
     modelMeanSqrtErr,
     "Rsquared score:",
     modelR2Score,
-    'Training score  : {}'.format(modelPrediction.score(X_train, y_train))),
-    'Test score      : {}'.format(modelPrediction.score(X_test, y_test))
+    "Training score:",
+    (model.score(X_train, y_train)),
+    "Test score:",
+    (model.score(X_test, y_test)),
+    "Cross-validated RMSE scores:",
+    rmse_scores,
 )
 
-print(modelPrediction)
+
+## Saving trained model
+with open("notinoAssignmentSalesPrediction.pkl", "wb") as file:
+    pickle.dump(predictionModel, file)
